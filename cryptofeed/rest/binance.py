5d3d915b3acd3ec1dec4155f2f654b5ccb5c6e2e
boseok-bc <boseok@blockcrafters.com>
2020-03-17 18:21:36 +0900

    Implement spot/margin create/keepalive_listen_key REST API

--- cryptofeed/rest/binance.py
+++ cryptofeed/rest/binance.py
@@ -1,3 +1,4 @@
+import os
 from time import sleep
 import time
 import hashlib
@@ -8,12 +9,14 @@ import zlib
 from datetime import datetime as dt
 from datetime import timedelta
 
+from dotenv import load_dotenv
 import requests
 
 from cryptofeed.rest.api import API, request_retry
 from cryptofeed.defines import BINANCE, SELL, BUY, BID, ASK
 from cryptofeed.standards import timestamp_normalize
 
+load_dotenv()
 LOG = logging.getLogger('rest')
 
 
@@ -24,20 +27,37 @@ class Binance(API):
     ID = BINANCE
     api = 'https://api.binance.com'
 
-    @staticmethod
-    def generate_signature(verb: str, url: str, data='', key_id=None, key_secret=None) -> dict:
+    def __init__(self):
+        self.key_id = os.getenv('BINANCE_API_KEY')
+        self.key_secret = os.getenv('BINANCE_SECRET_KEY')
+
+    def generate_signature(self, data='') -> dict:
         """
         verb: GET/POST/PUT
         url: api endpoint
         data: body (if present)
         """
-        pass
+        signature = hmac.new(bytes(self.key_secret, 'utf8'), bytes(data, 'utf8'), digestmod=hashlib.sha256).hexdigest()
+        return signature
 
-    def _post(self, command: str, payload=None):
-        pass
+    def _post(self, endpoint: str, data=None):
+        headers = {
+            'Accept': 'application/json',
+            'User-Agent': 'binance/python',
+            'X-MBX-APIKEY': self.key_id
+        }
+        response = requests.post(f'{self.api}{endpoint}', headers=headers, data=data)
+        print('response', response)
+        return response.json()
 
-    def _put(self, command: str, payload=None):
-        pass
+    def _put(self, endpoint: str, data=None):
+        headers = {
+            'Accept': 'application/json',
+            'User-Agent': 'binance/python',
+            'X-MBX-APIKEY': self.key_id
+        }
+        response = requests.put(f'{self.api}{endpoint}', headers=headers, data=data)
+        return response.json()
 
     def create_listen_key(self):
         """
@@ -50,10 +70,10 @@ class Binance(API):
            "listenKey": "pqia91ma19a5s61cv6a81va65sdf19v8a65a1a5s61cv6a81va65sdf19v8a65a1"
         }
         """
-        endpoint = ''
-        return requests.post(f'{self.api}{endpoint}')
+        endpoint = '/api/v3/userDataStream'
+        return self._post(endpoint)
 
-    def keepalive_listen_key(self):
+    def keepalive_listen_key(self, listen_key):
         """
         spot: PUT /api/v3/userDataStream
         margin: PUT /sapi/v1/userDataStream
@@ -64,4 +84,8 @@ class Binance(API):
            "listenKey": "pqia91ma19a5s61cv6a81va65sdf19v8a65a1a5s61cv6a81va65sdf19v8a65a1"
         }
         """
-        pass
+        endpoint = '/api/v3/userDataStream'
+        data = {
+            'listenKey': listen_key
+        }
+        return self._put(endpoint, data)
